64724f51ffb3e98857e822cd1a3b82e61339f542
EmanoJava <beatrizoliveirasantos646@gmail.com>
2024-08-21 11:58:03 -0300

    ACABOOOOOOOOOOOUUUUUUUUUUUUUU

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+ import { Footer } from "./components/Footer";
+import {Header} from "./components/Header";
+import { Main } from "./components/Main";
  
  
  function App() {
   return(
     <>
-    <h1>Titulo</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde corrupti neque officiis ullam error saepe nesciunt laboriosam voluptatem culpa id dolorum consequatur, alias molestiae deserunt, quaerat perspiciatis rem iure corporis.</p>
+    <Header />
+    <Main/>
+    <Footer/>
     </>
-  )
+  );
 }
 
 export {App} //quero embaixo
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+export function Main() {
+    return(
+        <><h1>Conteúdo</h1></>
+    )
+}
