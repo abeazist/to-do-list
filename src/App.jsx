641b2f5073db128dd2fec9f3b604cae5df54a9b3
EmanoJava <beatrizoliveirasantos646@gmail.com>
2024-08-21 11:00:04 -0300

    tudo limpo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+ 
+ 
+ function App() {
+  return(
+    <>
+    <h1>Titulo</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde corrupti neque officiis ullam error saepe nesciunt laboriosam voluptatem culpa id dolorum consequatur, alias molestiae deserunt, quaerat perspiciatis rem iure corporis.</p>
+    </>
+  )
+}
+
+export {App} //quero embaixo
