19a60ce57390b6dcab90fda75799531b3938b8ae
beatriz <128046453+abeazist@users.noreply.github.com>
2024-08-30 09:12:42 -0300

    produzindo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,17 @@ export function Main(props) {
     let { children } = props
     return (
         <div className={styles.Main}>
-            {children}
+            {/* {children} */}
+            <div class="box-1">
+                <input type="text" />
+                <button>+</button>
+            </div>
+            
+            <ul>
+                <li>Item 1 <input type='checkbox'/></li>
+                <li>Item 2 <input type='checkbox'/></li>
+                <li>Item 3 <input type='checkbox'/></li>
+            </ul>
         </div>
     )
 }
